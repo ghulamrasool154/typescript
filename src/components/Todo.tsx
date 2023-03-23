a695d05e478bd0d316c10e57544865c8e642bb9a
Ghulam Rasool <grsaddiqui75@gmail.com>
2023-03-23 23:01:06 +0500

    working on list of arrary

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,20 +1,34 @@
 import React, { useState } from "react";
 
 const Todo = () => {
-  const [item, setItems] = useState<[]>([]);
+  interface Myobj {
+    _id: string;
+    name: string;
+    age: number;
+  }
+
+  const [item, setItems] = useState<Myobj[]>([]);
   const [value, setvalue] = useState<string>("");
   const [age, setage] = useState<number>(20);
 
   const hanldeChanged = (element: React.ChangeEvent<HTMLInputElement>) => {
     setvalue(element.target.value);
   };
-  const hanldeAgeChanged = (e: React.ChangeEvent<HTMLInputElement>) => {
-    console.log(e);
+  const hanldeAgeChanged = (element: React.ChangeEvent<HTMLInputElement>) => {
+    setage(Number(element.target.value));
   };
   const hanldesubmit = (element: any) => {
     element.preventDefault();
-    console.log(item);
+
+    const newList = {
+      _id: `unique${Math.trunc(Math.random() * 1e9)}`,
+      name: value,
+      age: age,
+    };
+
+    setItems([...item, newList]);
   };
+  console.log(item);
   return (
     <div className="todo">
       <form onSubmit={hanldesubmit}>
@@ -30,6 +44,20 @@ const Todo = () => {
         />
         <button type="submit">Upload</button>
       </form>
+
+      {item.length <= 0 ? (
+        <h1>No User</h1>
+      ) : (
+        item.map((element) => {
+          return (
+            <h4 key={element._id}>
+              <strong> ID : {element._id}</strong>
+              <strong> Name : {element.name}</strong>
+              <strong> Age : {element.age}</strong>
+            </h4>
+          );
+        })
+      )}
     </div>
   );
 };
